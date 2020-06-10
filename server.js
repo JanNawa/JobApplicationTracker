8a453b8fbec33ff6b97c075ac21eb573b1e95621
Nawaphan Chayopathum <chayopat@sheridanc.on.ca>
2020-06-10 10:04:33 -0300

    update folder name to deploy to heroku

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/JobApplicationTracker'));
+app.use(express.static(__dirname + '/JobApplicationTracker'));
 
 app.get('/*', function (req, res) {
-    res.sendFile(path.join(__dirname + '/dist/JobApplicationTracker/index.html'));
+    res.sendFile(path.join(__dirname + '/JobApplicationTracker/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
